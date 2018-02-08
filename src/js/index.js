c305721d2bfb6eebf48000fccc5e633d85463aea
Максим <fotos544@gmail.com>
2018-02-08 14:54:45 +0300

    parallax_background

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,20 @@ import 'bootstrap';
 $(document).ready(function () {
     console.log('Webpack loaded!');
 });
+
+//background parallax effect
+
+$(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+
+    $("#bg-home").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 25;
+        var newvalueY = height * pageY * -1 - 50;
+        $('#bg-home').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    });
+
+});
